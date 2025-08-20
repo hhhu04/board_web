eb8a6e03d39d77726f081058360d38932f566a51
cat <hhhu04@gmail.com>
2025-08-20 23:37:53 +0900

    도커

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 중요: 루트 경로로 설정
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
